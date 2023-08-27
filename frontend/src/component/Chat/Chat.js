4feb072a3206466fc9bbd9eb3c735c6c86cb1474
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-28 03:22:37 +0530

    add scroll bar to it using react scroll to bottom

--- frontend/src/component/Chat/Chat.js
+++ frontend/src/component/Chat/Chat.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 const Chat = ({socket, username, room}) => {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -33,25 +34,32 @@ const Chat = ({socket, username, room}) => {
             <p>Live Chat</p>
         </div>
         <div className='chat-body'>
+            <ScrollToBottom className='message-container'>
             {messageList.map((messageContent)=>{
-                return <div className='message'>
+                return (<div className='message' 
+                id={username === messageContent.author ? "you" : "other"}>
                     <div>
                         <div className='message-content'>
                             <p>{messageContent.message}</p>
                         </div>
                         <div className='message-meta'>
-                            <p>{messageContent.time}</p>
-                            <p>{messageContent.author}</p>
+                            <p id="time">{messageContent.time}</p>
+                            <p id='author'>{messageContent.author}</p>
                         </div>
                     </div>
                 </div>
+            );
             })}
+            </ScrollToBottom>
         </div>
         <div className='chat-footer'>
             <input type='text' placeholder='Hey...' 
                 onChange={(event)=>{
                     setCurrentMessage(event.target.value)
                 }}
+                onKeyPress={(event) =>{
+                    event.key === "Enter" && sendMessage();
+            }}
             />
             <button onClick={sendMessage}>&#9658;</button>
         </div>
